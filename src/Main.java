eba065f1d04689c6732651a757eb00aec3d384f5
Erica <erica.abdao@zup.com.br>
2024-12-02 15:27:21 -0300

    add all project

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        String frase = "Hello world!";
+        int number = 10;
+        System.out.printf(frase);
+    }
+}
