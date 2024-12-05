33d056ce42de24e03ee779353e3cdc82168bc0f8
Erica <erica.abdao@zup.com.br>
2024-12-05 11:04:34 -0300

    retirar a variável int number = 10

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 public class Main {
     public static void main(String[] args) {
         String phrase = "Hello world!";
-        int number = 10;
-        System.out.printf(phrase);
+
+        System.out.println(phrase);
     }
 }
